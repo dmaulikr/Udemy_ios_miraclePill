4987380d8db36f9684eb65e1c9a512251e5bee4e
davidmgudeman <davidmgudeman@gmail.com>
2017-08-09 10:51:18 -0700

    all fields of main view are styled and the picker is working correctl

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
   @IBOutlet weak var statePicker: UIPickerView!
   @IBOutlet weak var statePickerBtn: UIButton!
@@ -17,10 +17,12 @@ class ViewController: UIViewController {
   @IBOutlet weak var countryLabel: UILabel!
   @IBOutlet weak var countryTxt: UITextField!
   
-  
+  var states = ["Alaska", "Alabama", "Arkansas", "California", "New York", "Maine"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    statePicker.dataSource = self
+    statePicker.delegate = self
   
     // Do any additional setup after loading the view, typically from a nib.
   }
@@ -29,8 +31,34 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return states.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return states[row]
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+    statePicker.isHidden = true
+    zipcodeLabel.isHidden = false
+    zipcodeTxt.isHidden = false
+    countryLabel.isHidden = false
+    countryTxt.isHidden = false
+
+  }
 
   @IBAction func stateBtnPressed(_ sender: AnyObject) {
+    statePicker.isHidden = false
+    zipcodeLabel.isHidden = true
+    zipcodeTxt.isHidden = true
+    countryLabel.isHidden = true
+    countryTxt.isHidden = true
   }
 
 }
